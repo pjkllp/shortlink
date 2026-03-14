96eaf4c8699baccb1ae50fe3f88749c38325a81d
pjk <2797857024@qq.com>
2026-03-14 12:49:13 +0800

    用户管理模块：添加了用户属性自动填充功能

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package com.nageoffer.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject,"createTime", Date.class,Date.from(Instant.now()));
+        this.strictInsertFill(metaObject,"updateTime",Date.class,Date.from(Instant.now()));
+        this.strictInsertFill(metaObject,"delFlag",Integer.class,0);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject,"updateTime",LocalDateTime.class,LocalDateTime.now());
+    }
+}
